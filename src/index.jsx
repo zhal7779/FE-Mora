0582df08128714ab2da1dca3ca9e559d3257d0be
Minyoung Lee <zhal7779@gmail.com>
2023-06-01 15:57:58 +0900

    Feat: 헤더 레이아웃 구현

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import ResetStyle from './ResetStyle';
+import Header from './components/header';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ResetStyle />
     <App />
+    <Header />
   </React.StrictMode>
 );
