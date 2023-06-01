551b8f0ab718b2a3530dbb9f7e5ebb679dcf37c3
Minyoung Lee <zhal7779@gmail.com>
2023-06-02 00:26:49 +0900

    Feat: 검색창 레이아웃 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, BrowserRouter as Router, Route } from 'react-router-dom';
+import SearchPage from './pages/SearchPage';
 
 const App = () => {
   return (
