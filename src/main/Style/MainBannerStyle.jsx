3049c4adbb1637122d6e5657b88a08faa6a09964
yvnji <yvnji25@gmail.com>
2023-06-02 04:24:09 +0900

    Feat: create main styled-components

---
+++ src/main/Style/MainBannerStyle.jsx
@@ -0,0 +1,31 @@
+export const BannerContainer = styled.section`
+  height: 925px;
+  background-color: #8e90ce;
+
+  .main-slogan {
+    display: flex;
+    justify-content: space-between;
+
+    width: 1280px;
+    margin: 0 auto;
+
+    &-side {
+      padding-top: 260px;
+
+      font-size: 4.8rem;
+      font-weight: 700;
+      color: #fff;
+      line-height: 6.3rem;
+
+      span {
+        font-size: 2.4rem;
+        font-weight: 400;
+        color: #fff;
+      }
+    }
+  }
+
+  .main-image {
+    padding-top: 100px;
+  }
+`;
