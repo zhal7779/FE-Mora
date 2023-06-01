ea5383158552eda8f804a03c4d58e12fc9af3a61
David9191 <jisung9105@gmail.com>
2023-06-01 23:37:37 +0900

    Chore: update path

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import { Routes, BrowserRouter as Router, Route } from 'react-router-dom';
-import AdminUser from './adminUser/pages/AdminUser';
-import Modal from './adminCommon/components/Modal';
 
 const App = () => {
   return (
     <React.StrictMode>
       <Router>
         <Routes>
-          <Route path='/modal' element={<Modal />}></Route>
-          <Route path='/' element={<AdminUser />}></Route>
-          <Route path='/admin/users' element={<AdminUser />}></Route>
+          <Route path='/'></Route>
         </Routes>
       </Router>
     </React.StrictMode>
