88499a99ce3bab87f363fa8fc12f197d2c96ced6
yvnji <yvnji25@gmail.com>
2023-06-02 04:27:15 +0900

    Feat: connect MainPage component to route

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { Routes, BrowserRouter as Router, Route } from 'react-router-dom';
+import MainPage from './pages/MainPage';
 
 const App = () => {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path='/' />
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </Router>
   );
 };
 
--- src/main/MainPosts.jsx
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-const MainPosts = () => {
-  return <section>메인포스트</section>;
-};
-
-export default MainPosts;
