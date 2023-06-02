c085ed90aa386409cf80fa92f7f4a66d552b884e
David9191 <jisung9105@gmail.com>
2023-06-02 19:51:28 +0900

    Refactor: 페이지네이션 컴포넌트 분리

--- src/adminUser/components/AdminTable.jsx
+++ src/adminUser/components/AdminTable.jsx
@@ -1,12 +1,6 @@
 import React from 'react';
-import {
-  PageNationBlock,
-  PageNationBlockDiv,
-  TableRowInfo,
-  UserInfo,
-  TableTitle,
-  DetailBtn,
-} from '../styledComponents/tableComponent';
+import { TableRowInfo, UserInfo, TableTitle, DetailBtn } from '../styledComponents/tableComponent';
+import PageNation from './PageNation';
 
 const tableColumnData = ['유저ID', '이름', '이메일', '비밀번호', '가입 날짜', '상세보기', '삭제'];
 
@@ -164,38 +158,13 @@ const AdminTableBody = ({ toggleModal }) => {
   );
 };
 
-// 나중에 util 함수로 빼도 될 듯(게시글 전체 렝스, 페이지당 보여줄 게시물 개수를 인자로 받아서 몇 페이지가 나올지 배열에 넣어서 반환하는 함수)
-const totalPages = [];
-const onePageCount = 12;
-let count = 1;
-let totalUser = tableBodyData.length;
-
-while (totalUser > 0) {
-  totalUser = ~~(totalUser / onePageCount);
-  totalPages.push(count++);
-}
-
-const PageCount = () => {
-  return (
-    <PageNationBlock className='page-nation-block'>
-      {totalPages.map((value) => {
-        return (
-          <PageNationBlockDiv className='page' key={value}>
-            <p>{value}</p>
-          </PageNationBlockDiv>
-        );
-      })}
-    </PageNationBlock>
-  );
-};
-
 const AdminTable = ({ toggleModal }) => {
   return (
     <div>
       <TableTitle className='table-title'>사용자 목록</TableTitle>
       <AdminTableHead />
       <AdminTableBody toggleModal={toggleModal} />
-      <PageCount />
+      <PageNation />
     </div>
   );
 };
