02a80a0d2792662007fef9c24b4b6a6080bb10f3
David9191 <jisung9105@gmail.com>
2023-06-04 02:54:33 +0900

    Feature: 페이지네이션 클릭 시 css 변경 기능 추가 + 코드리뷰 사항 적용

---
+++ src/admins/adminCommon/utils/pageNation.js
@@ -0,0 +1,42 @@
+import { PageNationBlockDiv } from '../../adminUser/styledComponents/tableComponent';
+
+/**
+ *
+ * @param {*} totalNumber 전체 컬럼(유저, 신고, 일정 등)개수
+ * @param {*} numberByPage 한 페이지당 보여줄 컬럼 개수
+ * @returns 페이지 개수
+ */
+export const getNumberOfPage = (totalNumber, numberByPage) => {
+  let count = 0;
+
+  if (totalNumber % numberByPage === 0) {
+    count = totalNumber / numberByPage;
+  } else {
+    count = totalNumber / numberByPage + 1;
+  }
+  return count;
+};
+
+/**
+ *
+ * @param {*} nowPageNumber 보여지도록 설정되는 페이지
+ * @param {*} numberOfPage 전체 페이지 개수
+ * @param {*} setNowPageNumber 각 버튼 컴포넌트 클릭 시 스테이트를 현재 페이지로 바꾸는 함수
+ * @returns 현재 페이지에 css가 적용된 전체 페이지 버튼
+ */
+export const getPageNumberComponents = (nowPageNumber, numberOfPage, setNowPageNumber) => {
+  const pageNumberComponents = [];
+
+  for (let index = 1; index <= numberOfPage; index++) {
+    pageNumberComponents.push(
+      <PageNationBlockDiv
+        className={index === nowPageNumber ? 'now-page page' : 'page'}
+        onClick={() => setNowPageNumber(index)}
+        key={index}
+      >
+        <p>{index}</p>
+      </PageNationBlockDiv>
+    );
+  }
+  return pageNumberComponents;
+};
--- src/admins/adminUser/components/PageNation.jsx
+++ src/admins/adminUser/components/PageNation.jsx
@@ -1,29 +1,18 @@
-import { PageNationBlock, PageNationBlockDiv } from '../styledComponents/tableComponent';
+import { useState } from 'react';
+import { PageNationBlock } from '../styledComponents/tableComponent';
+import { getPageNumberComponents, getNumberOfPage } from '../../adminCommon/utils/pageNation';
 
-// 나중에 util 함수로 빼도 될 듯(게시글 전체 렝스, 페이지당 보여줄 게시물 개수를 인자로 받아서 몇 페이지가 나올지 배열에 넣어서 반환하는 함수)
-const totalPages = [];
-const onePageCount = 12;
-let count = 1;
-// let totalUser = tableBodyData.length;
-let totalUser = 14;
+const PageNation = ({ totalDataNumber, numberByPage }) => {
+  const [nowPageNumber, setNowPageNumber] = useState(1);
 
-while (totalUser > 0) {
-  totalUser = ~~(totalUser / onePageCount);
-  totalPages.push(count++);
-}
-
-const PageNation = () => {
-  return (
-    <PageNationBlock className='page-nation-block'>
-      {totalPages.map((value) => {
-        return (
-          <PageNationBlockDiv className='page' key={value}>
-            <p>{value}</p>
-          </PageNationBlockDiv>
-        );
-      })}
-    </PageNationBlock>
+  const numberOfPage = getNumberOfPage(totalDataNumber, numberByPage);
+  const pageNumberComponents = getPageNumberComponents(
+    nowPageNumber,
+    numberOfPage,
+    setNowPageNumber
   );
+
+  return <PageNationBlock className='page-nation-block'>{pageNumberComponents}</PageNationBlock>;
 };
 
 export default PageNation;
