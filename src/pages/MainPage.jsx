8c1f4d0bca6d2d5a3973780407e4cb9d6566f51c
yvnji <yvnji25@gmail.com>
2023-06-05 01:51:48 +0900

    Fix: 메인페이지에 헤더, 푸터 연결

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,18 +1,18 @@
 import styled from 'styled-components';
-// import Header from '../components/Header';
-// import Footer from '../components/Footer';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 import MainBanner from '../main/MainBanner';
 import MainPost from '../main/MainPost';
 
 const MainPage = () => {
   return (
     <>
+      <Header />
       <MainContainer>
-        {/* <Header /> */}
         <MainBanner />
         <MainPost />
-        {/* <Footer /> */}
       </MainContainer>
+      <Footer />
     </>
   );
 };
@@ -20,6 +20,5 @@ const MainPage = () => {
 export default MainPage;
 
 const MainContainer = styled.div`
-  /* width: 100%;
-  height: 100vh; */
+  padding-top: 60px;
 `;
