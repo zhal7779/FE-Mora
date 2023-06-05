645378b9e5881347796d4c0985f36cc12e11250d
yvnji <yvnji25@gmail.com>
2023-06-06 01:54:20 +0900

    Feat: 카테고리 id 추가 및 카테고리 버튼에 게시글 작성페이지로 이동하는 링크 추가

--- src/community/Category.jsx
+++ src/community/Category.jsx
@@ -1,25 +1,21 @@
 import * as Style from './styledComponents/CategoryStyle';
-import { useState } from 'react';
 import Button from '../components/Button';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { categories } from './categoryData';
 
-const Category = () => {
-  // 현재 카테고리 상태
-  const [selectedCategory, setSelectedCategory] = useState('자유 게시판');
+const Category = ({ selectedCategory, setSelectedCategory }) => {
+  const { category_id } = useParams();
 
   const handleCategorySelect = category => {
-    setSelectedCategory(category); // 카테고리 상태 설정
-
-    // 선택된 카테고리에 해당하는 게시글 목록을 가져오는 로직 작성
+    setSelectedCategory(category);
   };
 
   return (
     <Style.CategoryContainer>
       <h2>토끼굴</h2>
-      <Button value="글 작성하기" color="darkPurple">
-        <Link to="/write"></Link>
-      </Button>
+      <Link to="/write">
+        <Button value="글 작성하기" color="darkPurple" />
+      </Link>
       <div className="category-list">
         <p className="category-list-title">토픽</p>
         <ul>
