f125f766c68d2c225e4ba13ce63615fdb1200cf2
yvnji <yvnji25@gmail.com>
2023-06-11 06:22:53 +0900

    Feat: 게시글 상세 컴포넌트로 파라미터 전달

--- src/pages/PostDetailPage.jsx
+++ src/pages/PostDetailPage.jsx
@@ -1,11 +1,14 @@
 import PostDetail from '../postDetail/PostDetail';
 import PostComment from './../postDetail/PostComment';
+import { useParams } from 'react-router-dom';
 
 const PostDetailPage = () => {
+  const { board_id } = useParams();
+
   return (
     <>
-      <PostDetail />
-      <PostComment />
+      <PostDetail postId={board_id} />
+      <PostComment postId={board_id} />
     </>
   );
 };
