9143b2aa3ba91f1a26e16af715e200af05c645b3
김지우 <ziuss76@gmail.com>
2023-06-11 03:25:14 +0900

    Chore: mypage error 해결

---
+++ src/myPage/MainProfile.jsx
@@ -0,0 +1,99 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useMutation } from 'react-query';
+import * as Style from './styledComponents/MyPageProfileStyle';
+import Button from '../components/Button';
+import Modal from '../components/Modal';
+import LoginInput from '../logIn/LogInInput';
+
+const MainProfile = ({ mainProfileData }) => {
+  const [showModal, setShowModal] = useState(false);
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  const deleteAccountMutation = useMutation(async () => {
+    const url = 'http://15.164.221.244:5000/api/users/delete';
+
+    const response = await fetch(url, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${sessionStorage.getItem('userToken')}`,
+      },
+      body: JSON.stringify({ password }),
+    });
+
+    const responseData = await response.json();
+    return responseData;
+  });
+
+  const handleDeleteAccount = async () => {
+    try {
+      await deleteAccountMutation.mutateAsync();
+      sessionStorage.removeItem('userToken');
+      closeModal();
+      navigate('/');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  console.log(mainProfileData);
+  return (
+    <Style.introContainer>
+      <div className='imgAndButtons'>
+        <img src={mainProfileData.userProfile.img_path} alt='프로필'></img>
+        <div className='buttons-container'>
+          <Style.ButtonLink to='/mypage/edit'>
+            <Button color='darkPurple' value='수정하기' />
+          </Style.ButtonLink>
+          <Button color='white' value='탈퇴하기' onClick={openModal} />
+        </div>
+      </div>
+      <h3>{mainProfileData.userName.name}</h3>
+      <h4>
+        {`${mainProfileData.userProfile.generation_id.split(' ')[0]} ${
+          mainProfileData.userProfile.generation_id.split(' ')[1]
+        }`}
+      </h4>
+      <h5>{mainProfileData.userProfile.position}</h5>
+      <div className='intro-container'>
+        <p className='intro'>
+          {mainProfileData.userProfile.comment ||
+            '수정하기 버튼을 눌러 간단한 자기소개를 입력해주세요!'}
+        </p>
+      </div>
+
+      {showModal && (
+        <Modal width='50rem'>
+          <div className='modal-container'>
+            <LoginInput
+              title='탈퇴하시려면 비밀번호를 입력해주세요'
+              type='password'
+              placeholder='비밀번호 입력'
+              name='password'
+              onChange={(e) => {
+                e.preventDefault();
+                setPassword(e.target.value);
+              }}
+              value={password}
+            />
+            <div className='modal-buttons-container'>
+              <Button color='white' value='취소' onClick={closeModal} />
+              <Button color='darkPurple' value='탈퇴' onClick={handleDeleteAccount} />
+            </div>
+          </div>
+        </Modal>
+      )}
+    </Style.introContainer>
+  );
+};
+
+export default MainProfile;
--- src/myPage/MyPageProfile.jsx
+++ src/myPage/MyPageProfile.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react';
-import styled from 'styled-components';
-import { useNavigate } from 'react-router-dom';
 import * as Style from './styledComponents/MyPageProfileStyle';
 import MainProfile from './MainProfile';
 import ProfileList from './ProfileList';
@@ -15,28 +12,3 @@ const MyPageProfile = ({ mainProfileData }) => {
 };
 
 export default MyPageProfile;
-
-const SkillButtonContainer = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  margin: 2rem 0;
-
-  .badge {
-    position: relative;
-    background-color: #ffffff;
-    border: 1px solid #b9b9b9;
-    padding: 8px 20px 9px 20px;
-    border-radius: 2rem;
-    margin-right: 1rem;
-    margin-bottom: 0.5rem;
-    font-size: 1.5rem;
-    cursor: pointer;
-  }
-`;
-
-const H5 = styled.h5`
-  font-weight: 400;
-  font-size: 1.7rem;
-  color: #424242;
-  line-height: 3rem;
-`;
