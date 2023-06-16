36b2190807dd21e850c610d2323426c4867bcdd5
yvnji <yvnji25@gmail.com>
2023-06-17 01:48:27 +0900

    Fix: postId가 없을 경우 대비 if문 추가

--- src/postDetail/api/apis.js
+++ src/postDetail/api/apis.js
@@ -1,17 +1,19 @@
 const BASE_URL = process.env.REACT_APP_URL;
 
 export const fetchPostDetail = async (postId) => {
-  const storedToken = sessionStorage.getItem('userToken');
-  const response = await fetch(`${BASE_URL}/api/boards/detail/${postId}`, {
-    headers: {
-      authorization: `Bearer ${storedToken}`,
-    },
-  });
-  if (!response.ok) {
-    throw new Error('게시글 상세를 불러오는데 실패했습니다.');
+  if (postId) {
+    const storedToken = sessionStorage.getItem('userToken');
+    const response = await fetch(`${BASE_URL}/api/boards/detail/${postId}`, {
+      headers: {
+        authorization: `Bearer ${storedToken}`,
+      },
+    });
+    if (!response.ok) {
+      throw new Error('게시글 상세를 불러오는데 실패했습니다.');
+    }
+    const data = await response.json();
+    return data;
   }
-  const data = await response.json();
-  return data;
 };
 
 export const fetchLike = async (postId) => {
