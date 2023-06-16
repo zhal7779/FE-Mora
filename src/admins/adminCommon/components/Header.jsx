a39d9a16c149a5163324ce27c1d6ca3b7daa829d
David9191 <jisung9105@gmail.com>
2023-06-16 14:56:38 +0900

    Design: 관리자 헤더 레이아웃

--- src/admins/adminCommon/components/Header.jsx
+++ src/admins/adminCommon/components/Header.jsx
@@ -1,17 +1,42 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import DefaultImg from '../../../assets/images/rabbitProfile.png';
+import { ReactComponent as LogoIcon } from '../../../assets/icons/logo1.svg';
+
+const Container = styled.header`
+  position: fixed;
+  display: block;
+  left: 0;
+  right: 0;
+  top: 0;
+  height: 6rem;
+
+  border-bottom: #cbd5e1 1px solid;
+  background: #ffffff;
+`;
+const Content = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  max-width: 1280px;
+  height: 100%;
+  margin: 0 auto;
+`;
+const MenuContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
 const Header = () => {
   return (
-    <header
-      style={{
-        height: '55px',
-        backgroundColor: 'pink',
-
-        fontSize: '24px',
-        textAlign: 'center',
-        lineHeight: '55px',
-      }}
-    >
-      Header
-    </header>
+    <Container>
+      <Content>
+        <MenuContainer>
+          <Link to='/admin/users'>
+            <LogoIcon />
+          </Link>
+        </MenuContainer>
+      </Content>
+    </Container>
   );
 };
 
