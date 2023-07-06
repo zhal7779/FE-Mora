93832bc04c0f608007d884e294ce57ceba522e6e
yvnji <yvnji25@gmail.com>
2023-07-06 21:50:43 +0900

    !HOTFIX: 게시글 조회 api 변경에 의해 게시글 필터링 api 추가적으로 필요

--- src/community/RecommendPost.jsx
+++ src/community/RecommendPost.jsx
@@ -1,8 +1,53 @@
 import * as Style from './styledComponents/RecommendPostStyle';
+import { useQuery } from 'react-query';
+import Button from '../components/Button';
+const BASE_URL = process.env.REACT_APP_URL;
 import { Link } from 'react-router-dom';
 
-const RecommendPost = ({ searchTerm, selectedCategoryId, data }) => {
-  const sortedData = data
+const RecommendPost = ({ searchTerm, selectedCategoryId }) => {
+  const fetchPosts = async () => {
+    const response = await fetch(
+      `${BASE_URL}/api/boards/${selectedCategoryId}?page=0&size=100`,
+      {
+        headers: {
+          authorization: `Bearer ${sessionStorage.getItem('userToken')}`
+        }
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error('게시글을 불러오는데 실패했습니다.');
+    }
+
+    const result = await response.json();
+    return result;
+  };
+
+  const { data, isLoading, isError, error, isSuccess } = useQuery(
+    ['posts', selectedCategoryId],
+    fetchPosts
+  );
+
+  if (isError) {
+    return (
+      <div>
+        {error.message}⚠️
+        <div>
+          모여라레이서는 회원 전용 서비스입니다! <br />
+          혹시 로그인을 깜빡하셨나요?
+          <Link to="/login">
+            <Button value="로그인하기" color="darkPurple" />
+          </Link>
+        </div>
+      </div>
+    );
+  }
+
+  if (isLoading) {
+    return <div>Loading...⏳</div>;
+  }
+
+  const sortedData = data.objArr
     .filter(post => post.category === selectedCategoryId)
     .sort((a, b) => b.like_cnt + b.view_cnt - (a.like_cnt + a.view_cnt));
 
@@ -10,7 +55,7 @@ const RecommendPost = ({ searchTerm, selectedCategoryId, data }) => {
 
   return (
     <Style.RecommendContainer>
-      {searchTerm === '' && recommendPostData.length > 0 ? (
+      {searchTerm === '' && isSuccess ? (
         <>
           <h3>추천 게시글</h3>
           <ul>
--- src/pages/CommunityPage.jsx
+++ src/pages/CommunityPage.jsx
@@ -5,68 +5,12 @@ import Category from '../community/Category';
 import SearchBar from '../community/SearchBar';
 import PostList from '../community/PostList';
 import RecommendPost from '../community/RecommendPost';
-import { useQuery } from 'react-query';
-import { Link } from 'react-router-dom';
-import Button from '../components/Button';
-const BASE_URL = process.env.REACT_APP_URL;
 
 const CommunityPage = () => {
   const [selectedCategoryId, setSelectedCategoryId] = useState(
     categories[0].id
   );
   const [searchTerm, setSearchTerm] = useState('');
-
-  const fetchPosts = async () => {
-    const response = await fetch(
-      `${BASE_URL}/api/boards/${selectedCategoryId}?page=0&size=100`,
-      {
-        headers: {
-          authorization: `Bearer ${sessionStorage.getItem('userToken')}`
-        }
-      }
-    );
-
-    if (!response.ok) {
-      throw new Error('게시글을 불러오는데 실패했습니다.');
-    }
-
-    const result = await response.json();
-    return result.objArr;
-  };
-
-  const { data, isLoading, isError, error } = useQuery(
-    ['posts', selectedCategoryId],
-    fetchPosts,
-    {
-      // 페이지네이션을 위한 query key 변경
-      getNextPageParam: (lastPage, allPages) => {
-        // 현재 페이지가 마지막 페이지가 아니라면 다음 페이지 번호를 반환하고,
-        // 마지막 페이지라면 null을 반환하여 페이지네이션을 종료.
-        return lastPage.currentPage < lastPage.totalPages
-          ? lastPage.currentPage + 1
-          : null;
-      }
-    }
-  );
-
-  if (isError) {
-    return (
-      <Status>
-        {error.message}⚠️
-        <div>
-          모여라레이서는 회원 전용 서비스입니다! <br />
-          혹시 로그인을 깜빡하셨나요?
-          <Link to="/login">
-            <Button value="로그인하기" color="darkPurple" />
-          </Link>
-        </div>
-      </Status>
-    );
-  }
-
-  if (isLoading) {
-    return <Status>Loading...⏳</Status>;
-  }
 
   return (
     <CommunityContainer>
@@ -76,14 +20,12 @@ const CommunityPage = () => {
       />
       <PostContainer>
         <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-        <RecommendPost
+        {/* <RecommendPost
           searchTerm={searchTerm}
           selectedCategoryId={selectedCategoryId}
-          data={data}
-        />
+        /> */}
         <PostList
           selectedCategoryId={selectedCategoryId}
-          data={data}
           searchTerm={searchTerm}
         />
       </PostContainer>
