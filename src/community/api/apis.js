a5142a519ffa4b7b9df389c0a7ee6b412e1dbbf8
yvnji <yvnji25@gmail.com>
2023-07-08 02:24:12 +0900

    Fix: 추천 게시글 api 연결 및 추가 수정

--- src/community/api/apis.js
+++ src/community/api/apis.js
@@ -1,9 +1,9 @@
 const BASE_URL = process.env.REACT_APP_URL;
 
 // 게시글 리스트 조회 api
-export const fetchPosts = async ({ selectedCategoryId, page, view, keyword }) => {
+export const fetchPosts = async ({ selectedCategoryId, page, view, keyword, sort }) => {
   const response = await fetch(
-    `${BASE_URL}/api/boards/${selectedCategoryId}?page=${page}&size=${view}&keyword=${keyword}`,
+    `${BASE_URL}/api/boards/${selectedCategoryId}?page=${page}&size=${view}&keyword=${keyword}&sort=${sort}`,
     {
       headers: {
         authorization: `Bearer ${sessionStorage.getItem('userToken')}`,
@@ -20,15 +20,11 @@ export const fetchPosts = async ({ selectedCategoryId, page, view, keyword }) =>
 };
 
 // 추천 게시글 조회 api
-export const fetchRecommendPosts = async () => {
-  const response = await fetch(`${BASE_URL}/api/boards/${selectedCategoryId}?page=0&size=100`, {
-    headers: {
-      authorization: `Bearer ${sessionStorage.getItem('userToken')}`,
-    },
-  });
+export const fetchRecommendPosts = async (selectedCategoryId) => {
+  const response = await fetch(`${BASE_URL}/api/boards/${selectedCategoryId}/recommend`);
 
   if (!response.ok) {
-    throw new Error('게시글을 불러오는데 실패했습니다.');
+    throw new Error('추천 게시글을 불러오는데 실패했습니다.');
   }
 
   const result = await response.json();
