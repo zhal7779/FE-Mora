aeba86d1a0e27c117ffc03a4b11ad20a9a30d4ff
yvnji <yvnji25@gmail.com>
2023-07-11 16:14:20 +0900

    Chore: tsconfig.json 수정

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,9 @@ import App from './App';
 import ResetStyle from './ResetStyle';
 import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
   <React.StrictMode>
     <ResetStyle />
