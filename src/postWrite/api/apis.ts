b7ea38ce390e1b0b84ff18c5ba97684cba8d72d3
yvnji <yvnji25@gmail.com>
2023-07-14 05:08:00 +0900

    Refactor: 게시글 등록/수정 및 이미지 등록 api 타입 지정

--- src/postWrite/api/apis.ts
+++ src/postWrite/api/apis.ts
@@ -1,38 +1,46 @@
+import {
+  imageData,
+  registerPostParams,
+  responseFormData
+} from '../types/types';
 const BASE_URL = process.env.REACT_APP_URL;
 
 // 게시글 등록/수정 api
-export const registerPost = async (postId, postData) => {
+export const registerPost = async ({
+  postId,
+  postData
+}: registerPostParams): Promise<responseFormData> => {
   const response = await fetch(`${BASE_URL}/api/boards`, {
     method: postId ? 'PUT' : 'POST',
     body: JSON.stringify(postData),
     headers: {
       'Content-Type': 'application/json',
-      authorization: `Bearer ${sessionStorage.getItem('userToken')}`,
-    },
+      authorization: `Bearer ${sessionStorage.getItem('userToken')}`
+    }
   });
 
   if (!response.ok) {
     throw new Error('게시글 등록에 실패하였습니다.');
   }
 
-  const result = response.json();
+  const result: responseFormData = await response.json();
   return result;
 };
 
 // 이미지 등록 api
-export const postImage = async (imgFormData) => {
+export const postImage = async (imgFormData: FormData): Promise<imageData> => {
   const response = await fetch(`${BASE_URL}/api/boards/img`, {
     method: 'POST',
     body: imgFormData,
     headers: {
-      authorization: `Bearer ${sessionStorage.getItem('userToken')}`,
-    },
+      authorization: `Bearer ${sessionStorage.getItem('userToken')}`
+    }
   });
 
   if (!response.ok) {
     throw new Error('이미지 등록에 실패하였습니다.');
   }
 
-  const result = await response.json();
+  const result: imageData = await response.json();
   return result;
 };
