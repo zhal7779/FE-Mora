fecc9ff870b2ab7bec3f0fbf9de5dd991e6706a1
yvnji <yvnji25@gmail.com>
2023-07-15 01:36:34 +0900

    Refactor: 커뮤니티 게시글 상세페이지 api 코드 전체 타입 적용

--- src/postDetail/api/apis.ts
+++ src/postDetail/api/apis.ts
@@ -1,46 +1,32 @@
+import { commentData, communityPostData } from '../types/types';
 const BASE_URL = process.env.REACT_APP_URL;
 
 // 게시글 상세 조회 api
-export const fetchPostDetail = async (postId) => {
-  if (postId) {
-    const storedToken = sessionStorage.getItem('userToken');
-    const response = await fetch(`${BASE_URL}/api/boards/detail/${postId}`, {
-      headers: {
-        authorization: `Bearer ${storedToken}`,
-      },
-    });
-    if (!response.ok) {
-      throw new Error('게시글 상세를 불러오는데 실패했습니다.');
-    }
-    const data = await response.json();
-    return data;
-  }
-};
-
-// 게시글 좋아요 수 조회 api
-export const fetchLike = async (postId) => {
+export const fetchPostDetail = async (
+  postId: string
+): Promise<communityPostData> => {
   const storedToken = sessionStorage.getItem('userToken');
-  const response = await fetch(`${BASE_URL}/api/likes/${postId}`, {
+  const response = await fetch(`${BASE_URL}/api/boards/detail/${postId}`, {
     headers: {
-      authorization: `Bearer ${storedToken}`,
-    },
+      authorization: `Bearer ${storedToken}`
+    }
   });
   if (!response.ok) {
-    throw new Error('게시글 좋아요 수를 불러오는데 실패했습니다.');
+    throw new Error('게시글 상세를 불러오는데 실패했습니다.');
   }
-  const data = await response.json();
+  const data: communityPostData = await response.json();
   return data;
 };
 
 // 게시글 삭제 api
-export const deletePost = async (postId) => {
+export const deletePost = async (postId: string) => {
   const response = await fetch(`${BASE_URL}/api/boards`, {
     method: 'DELETE',
     headers: {
       'Content-Type': 'application/json',
-      authorization: `Bearer ${sessionStorage.getItem('userToken')}`,
+      authorization: `Bearer ${sessionStorage.getItem('userToken')}`
     },
-    body: JSON.stringify({ board_id: postId }),
+    body: JSON.stringify({ board_id: postId })
   });
   if (!response.ok) {
     throw new Error('게시글 삭제에 실패했습니다.');
@@ -50,14 +36,20 @@ export const deletePost = async (postId) => {
 };
 
 // 좋아요 등록/취소 api
-export const toggleLike = async (userLike, postId) => {
+export const toggleLike = async ({
+  userLike,
+  postId
+}: {
+  userLike: boolean;
+  postId: string;
+}) => {
   const response = await fetch(`${BASE_URL}/api/likes`, {
     method: userLike ? 'DELETE' : 'POST',
     headers: {
       'Content-Type': 'application/json',
-      authorization: `Bearer ${sessionStorage.getItem('userToken')}`,
+      authorization: `Bearer ${sessionStorage.getItem('userToken')}`
     },
-    body: JSON.stringify({ board_id: postId }),
+    body: JSON.stringify({ board_id: postId })
   });
   if (!response.ok) {
     throw new Error('좋아요 처리에 실패했습니다.');
@@ -67,49 +59,58 @@ export const toggleLike = async (userLike, postId) => {
 };
 
 //댓글 조회 api
-export const fetchComments = async (postId) => {
-  const response = await fetch(`${BASE_URL}/api/boards/detail/${postId}/comments`, {
-    headers: {
-      authorization: `Bearer ${sessionStorage.getItem('userToken')}`,
-    },
-  });
+export const fetchComments = async (postId: string): Promise<commentData[]> => {
+  const response = await fetch(
+    `${BASE_URL}/api/boards/detail/${postId}/comments`,
+    {
+      headers: {
+        authorization: `Bearer ${sessionStorage.getItem('userToken')}`
+      }
+    }
+  );
 
   if (!response.ok) {
     throw new Error('댓글을 불러오는데 실패했습니다.');
   }
 
-  const result = await response.json();
+  const result: commentData[] = await response.json();
   return result;
 };
 
 // 댓글 등록/수정 api
-export const postComment = async (registerData, editCommentId) => {
+export const postComment = async ({
+  registerData,
+  editCommentId
+}: {
+  registerData: object;
+  editCommentId: string | null;
+}): Promise<commentData> => {
   const response = await fetch(`${BASE_URL}/api/comments`, {
     method: editCommentId ? 'PATCH' : 'POST',
     headers: {
       'Content-Type': 'application/json',
-      authorization: `Bearer ${sessionStorage.getItem('userToken')}`,
+      authorization: `Bearer ${sessionStorage.getItem('userToken')}`
     },
-    body: JSON.stringify(registerData),
+    body: JSON.stringify(registerData)
   });
 
   if (!response.ok) {
     throw new Error('댓글 등록에 실패하였습니다.');
   }
 
-  const result = await response.json();
+  const result: commentData = await response.json();
   return result;
 };
 
 // 댓글 삭제 api
-export const deleteComment = async (commentId) => {
+export const deleteComment = async (commentId: string) => {
   const response = await fetch(`${BASE_URL}/api/comments`, {
     method: 'DELETE',
     headers: {
       'Content-Type': 'application/json',
-      authorization: `Bearer ${sessionStorage.getItem('userToken')}`,
+      authorization: `Bearer ${sessionStorage.getItem('userToken')}`
     },
-    body: JSON.stringify({ comment_id: commentId }),
+    body: JSON.stringify({ comment_id: commentId })
   });
   if (!response.ok) {
     throw new Error('댓글 삭제에 실패했습니다.');
