0bdc679adb8a97a2ba8754abf43721d708b39de0
Minyoung Lee <zhal7779@gmail.com>
2023-07-21 04:41:19 +0900

    Fix: useObserver 훅  ts 변환

--- src/hooks/useObserver.ts
+++ src/hooks/useObserver.ts
@@ -1,6 +1,10 @@
 import React, { useEffect } from 'react';
 //무한스크롤 DOM요소 가시성 감지 함수
-export const useObserver = (observerRef, fetchNextPage, hasNextPage) => {
+export const useObserver = (
+  observerRef: React.RefObject<HTMLDivElement>,
+  fetchNextPage: () => void,
+  hasNextPage: boolean = false
+) => {
   useEffect(() => {
     const observer = new IntersectionObserver(
       ([entry]) => {
