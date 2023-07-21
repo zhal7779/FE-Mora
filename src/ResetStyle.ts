80fc4162d59632fe9be556e91080d1d5dc47df9f
yvnji <yvnji25@gmail.com>
2023-07-21 23:05:57 +0900

    Refactor: 텍스트 숨기기 위한 CSS 코드 추가

--- src/ResetStyle.ts
+++ src/ResetStyle.ts
@@ -199,6 +199,17 @@ button {
   cursor: pointer;
 }
 
+.hide {
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  margin: -1px;
+  overflow: hidden;
+  clip: rect(0, 0, 0, 0);
+  white-space: nowrap;
+  border: 0;
+}
 
 @media (max-width: 375px) {
   html {
